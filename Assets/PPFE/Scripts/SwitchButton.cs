0df1e243fbc21fb6238023fff6ea8e3e3ec51283
Badaril <120525653+Badaril@users.noreply.github.com>
2026-05-13 12:48:51 +0200

    main menu
    and finally fix canvas bug

---
+++ Assets/PPFE/Scripts/SwitchButton.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SwitchButton : MonoBehaviour, IPointerClickHandler
+{
+    public RectTransform handle; // La pastille qui bouge
+    public Color onColor;
+    public Color offColor;
+    public Image background;
+    private bool isOn = true;
+    private Vector2 offPos, onPos;
+
+    void Start()
+    {
+        offPos = handle.anchoredPosition;
+        onPos = offPos + new Vector2 (handle.rect.width, 0); // Ajustez selon la taille du bouton
+        UpdateSwitch();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        isOn = !isOn;
+        UpdateSwitch();
+    }
+
+    void UpdateSwitch()
+    {
+        handle.anchoredPosition = isOn ? onPos : offPos;
+        background.color = isOn ? onColor : offColor;
+        //Debug.Log("Switch : " + (isOn ? "ON" : "OFF"));
+    }
+}
